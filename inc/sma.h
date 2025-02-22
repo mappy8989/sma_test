f2dce7e08e7fada6c0f342f75fee0919d8004bb7
IgorF <kleef@mail.ru>
2025-02-22 12:19:01 +0300

    Create sma_calc() function
    - check sma_calc() for time execution

--- inc/sma.h
+++ inc/sma.h
@@ -4,10 +4,28 @@
 #include <vector>
 
 template <typename type>
-void sma(std::vector<type> data_in, int window_size, std::vector<type> data_out)
+std::vector<type> sma_calc(std::vector<type> data_in, int window_size)
 {
-    if (data_in.size() < window_size) {
-        std::cout << "Input data size should be >= window_size" << std::endl;
-        return;
+    if (data_in.size() < window_size || data_in.empty() || !window_size) {
+        std::cout << "Incorrect input data" << std::endl;
+        return {};
     }
+
+    std::vector<type> data_out(data_in.size() - window_size + 1);
+
+    // calculate first mean for 0 to window_size data points
+    type sma_first = 0;
+    for (int i = 0; i < window_size; i++) {
+        sma_first += data_in[i];
+    }
+    sma_first      = sma_first / window_size;
+    data_out.at(0) = sma_first;
+
+    for (int i = 1; i < data_out.size(); i++) {
+        data_out.at(i) = data_out.at(i - 1) +
+                         (data_in.at(window_size - 1 + i) - data_in.at(i - 1)) /
+                             window_size;    // according to formula from wiki
+    }
+
+    return data_out;
 }
--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
+#include "inc/sma.h"
 #include <chrono>
 #include <iostream>
+#include <random>
 #include <thread>
 
 int main(void)
 {
-    auto start_time = std::chrono::system_clock::now();
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    auto end_time = std::chrono::system_clock::now();
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(0.0, 1000.0);
+
+    std::vector<double> data_in(1000000);
+    for (int i = 0; i < data_in.size(); i++) {
+        data_in.at(i) = dis(gen);
+    }
+
+    auto start_time         = std::chrono::system_clock::now();
+    std::vector<double> sma = sma_calc(data_in, 16);
+    auto end_time           = std::chrono::system_clock::now();
 
     auto differenceUs = std::chrono::duration_cast<std::chrono::microseconds>(
                             end_time - start_time)
                             .count();
 
-    std::cout << differenceUs / 1000000 << std::endl;
+    std::cout << differenceUs << std::endl;
 
     return 0;
 }
